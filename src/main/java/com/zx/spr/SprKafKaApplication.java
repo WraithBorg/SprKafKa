1a86c47237e598e65d0d62755b6dbaa562b81897
Thanos <2325098522@qq.com>
2020-09-08 13:08:52 +0800

    Initial commit

---
+++ src/main/java/com/zx/spr/SprKafKaApplication.java
@@ -0,0 +1,13 @@
+package com.zx.spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprKafKaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprKafKaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zx/spr/SprKafKaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zx.spr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprKafKaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
